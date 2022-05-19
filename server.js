6e86fec681c115b78a17492a4e54e9c565aaf982
MrVoze <ecarlson0123@yahoo.com>
2022-05-19 00:20:43 -0500

    post and delete request

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
-
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./db/db');
+app.use(express.json());
+app.use(express.static('public'));
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    console.log(note);
+    return note;
+};
 
 function filterByQuery(query, notesArray) {
     let filteredResults = notesArray;
@@ -19,6 +33,23 @@ function findById(id, notesArray) {
     const result = notesArray.filter(note => note.id === id)[0];
     return result;
 };
+
+app.delete('/api/notes/:id', (req, res) => {
+    let noteToDelete = notes.findIndex((value)=> {
+        return value.id === req.params.id
+    });
+    notes.splice(noteToDelete, 1)
+    res.json('note removed');
+    res.status = 200
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.get('/api/notes', (req, res) => {
     let results = notes;
@@ -37,16 +68,24 @@ app.get('/api/notes/:id', (req, res) => {
     }
 });
 
-router.post('/notes', (req, res) => {
+app.post('/api/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    req.body.id = notes.length.toString();
-  
-    if (!validateNote(req.body)) {
-      res.status(400).send('The note is not properly formatted.');
-    } else {
-      const note = createNewNote(req.body, notes);
-      res.json(note);
+    highestIndex = 0
+    for(const note in notes){
+        console.log(note);
+        if(notes[note].id>highestIndex){
+            newIndex = (Number(notes[note].id)+1);
+            console.log(newIndex)
+            highestIndex = newIndex;
+        }
+        else {
+            highestIndex +=1
+        }
     }
+    req.body.id = highestIndex.toString();
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+
 });
   
 
