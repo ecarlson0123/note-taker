afd2e3a45b724fedf981d17f8a0450c5a5e11f45
MrVoze <ecarlson0123@yahoo.com>
2022-05-18 22:47:02 -0500

    get requests

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ function filterByQuery(query, notesArray) {
     return filteredResults;
 };
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+};
+
 app.get('/api/notes', (req, res) => {
     let results = notes;
     if (req.query) {
@@ -22,6 +27,28 @@ app.get('/api/notes', (req, res) => {
     }
     res.json(results);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+    } else {
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+    }
+});
+  
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
